6a50490cc73dcc4d2fdca8dd9417e9b2f8646c6c
bertram <bertram.skarupa@gmail.com>
2020-05-27 21:20:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("c19").addEventListener("click", function(){
+  window.open('https://c19model.com');
+});
+document.getElementById("mb").addEventListener("click", function(){
+  window.open('https://chrome.google.com/webstore/detail/managebac-grade-calculato/iccmimianhoklepafpckijcpclgcfblm');
+});
+document.getElementById("ftc").addEventListener("click", function(){
+  window.open('https://poweredbyredstone.team');
+});
+document.getElementById("github").addEventListener("click", function(){
+  window.open('https://github.com/Bertik750');
+});
+document.getElementById("in").addEventListener("click", function(){
+  window.open('https://www.linkedin.com/in/bertramskarupa');
+});

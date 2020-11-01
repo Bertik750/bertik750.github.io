5cd2cc4fdee23c6e6630975ad022c1879f873ae1
bertram <bertram.skarupa@gmail.com>
2020-11-01 13:00:26 +0100

    New version with more information. Added Radio

--- index.js
+++ index.js
@@ -7,9 +7,39 @@ document.getElementById("mb").addEventListener("click", function(){
 document.getElementById("ftc").addEventListener("click", function(){
   window.open('https://poweredbyredstone.team');
 });
-document.getElementById("github").addEventListener("click", function(){
-  window.open('https://github.com/Bertik750');
+document.getElementById("ftc").addEventListener("click", function(){
+  window.open('https://poweredbyredstone.team');
+});
+document.getElementById("kavarna").addEventListener("click", function(){
+  window.open('https://github.com/Bertik750/cafeSystem');
 });
 document.getElementById("in").addEventListener("click", function(){
   window.open('https://www.linkedin.com/in/bertramskarupa');
 });
+document.getElementById("cv").addEventListener("click", function(){
+  window.open('https://drive.google.com/file/d/1tkMLevh2bcDAfh89piOGNmgdNhKrUTAQ/view');
+});
+
+document.getElementById("aktualne").addEventListener("click", function(){
+  window.open('https://magazin.aktualne.cz/dobre-zpravy/simulace-koronavir/r~b0e6dd6c78af11ea9d470cc47ab5f122/');
+});
+document.getElementById("denik").addEventListener("click", function(){
+  window.open('https://www.denik.cz/regiony/koronavirus-student-gymnazium-simulace-nakaza-20200403.html');
+});
+document.getElementById("bobr").addEventListener("click", function(){
+  window.open('https://www.ibobr.cz/kontakt');
+});
+document.getElementById("blesk").addEventListener("click", function(){
+  window.open('https://www.blesk.cz/clanek/zpravy-koronavirus/640804/student-18-vytvoril-hravou-aplikaci-ktera-simuluje-sireni-koronaviru-pochopil-to-i-muj-8lety-bratr-rika.html');
+});
+document.getElementById("prague").addEventListener("click", function(){
+  window.open('https://www.praguemorning.cz/czech-student-created-virus-spread-simulation/');
+});
+document.getElementById("prima").addEventListener("click", function(){
+  window.open('https://prima.iprima.cz/karantena/3-epizoda');
+});
+document.getElementById("stredoskolak").addEventListener("click", function(){
+  window.open('https://www.prostredoskolaky.cz/finaliste');
+});
+
+
